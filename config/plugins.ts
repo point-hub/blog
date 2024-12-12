6e5da5eb2ac9778f459aa50c72ee8afb94302b4b
Martien Dermawan Tanama <martiendt@gmail.com>
2024-12-12 09:29:33 +0700

    feat: add plugin mailgun

--- config/plugins.ts
+++ config/plugins.ts
@@ -25,4 +25,18 @@ export default ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: 'mailgun',
+      providerOptions: {
+        key: env('MAILGUN_API_KEY'),
+        domain: env('MAILGUN_DOMAIN'),
+        url: env('MAILGUN_URL', 'https://api.mailgun.net'),
+      },
+      settings: {
+        defaultFrom: env('MAIL_FROM'),
+        defaultReplyTo: env('MAIL_REPLY_TO'),
+      },
+    },
+  },
 });
